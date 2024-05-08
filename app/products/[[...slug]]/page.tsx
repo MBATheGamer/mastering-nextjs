fc1d848ebc6450d9f5c50dfd93427b0647c1dfcc
MBATheGamer <mbathegamer@gmail.com>
2024-05-08 23:42:17 +0100

    Accessing query string parameters

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,9 +1,17 @@
 type Props = {
   params: { slug: string[] };
+  searchParams: { sortOrder: string };
 };
 
-const ProductPage = ({ params: { slug } }: Props) => {
-  return <div>Product Page {slug}</div>;
+const ProductPage = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
+  return (
+    <div>
+      Product Page {slug} {sortOrder}
+    </div>
+  );
 };
 
 export default ProductPage;
