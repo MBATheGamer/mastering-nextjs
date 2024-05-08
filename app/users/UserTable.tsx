4140b17c4ca0747ffe9c69d572f3a8cffbca3ffd
MBATheGamer <mbathegamer@gmail.com>
2024-05-09 00:08:58 +0100

    Accessing query string parameters (Exercise)

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,26 +1,51 @@
+import { sort } from "fast-sort";
+import Link from "next/link";
+
 type User = {
   id: number;
   name: string;
   email: string;
 };
 
-const UserTable = async () => {
+type Props = {
+  sortBy?: string;
+};
+
+const UserTable = async ({ sortBy }: Props) => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users", {
     cache: "no-store",
   });
 
   const users: User[] = await response.json();
 
+  let sortedUser: User[];
+
+  switch (sortBy) {
+    case "name":
+      sortedUser = sort(users).asc(user => user.name);
+      break;
+    case "email":
+      sortedUser = sort(users).asc(user => user.email);
+      break;
+    default:
+      sortedUser = users;
+      break;
+  }
+
   return (
     <table className="table">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
+          <th>
+            <Link href="/users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {users.map(user => (
+        {sortedUser.map(user => (
           <tr key={user.id}>
             <td>{user.name}</td>
             <td>{user.email}</td>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,14 @@
 import UserTable from "./UserTable";
 
-const UsersPage = async () => {
+type Props = {
+  searchParams: { sortOrder: string };
+};
+
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
-      <UserTable />
+      <UserTable sortBy={sortOrder} />
     </>
   );
 };
