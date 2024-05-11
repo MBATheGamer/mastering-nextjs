2e85f2ebe247a4fae89533027c9214d8899beeb8
MBATheGamer <mbathegamer@gmail.com>
2024-05-11 17:32:51 +0100

    Update an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,15 @@ export const GET = (request: NextRequest, { params }: Props) => {
 
   return NextResponse.json({ id: params.id, name: "Mohamed" });
 };
+
+export const PUT = async (request: NextRequest, { params }: Props) => {
+  const body = await request.json();
+
+  if (!body["name"])
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: params.id, name: body["name"] });
+};
