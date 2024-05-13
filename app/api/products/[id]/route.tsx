a9901b6b4544e599e8885bca7db2eead49d9ff0a
MBATheGamer <mbathegamer@gmail.com>
2024-05-13 01:34:23 +0100

    Storing products in database (get data by id)

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -1,15 +1,22 @@
+import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
 
 type Props = {
-  params: { id: number };
+  params: { id: string };
 };
 
-export const GET = (request: NextRequest, { params }: Props) => {
-  if (params.id > 10)
+export const GET = async (request: NextRequest, { params }: Props) => {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!product)
     return NextResponse.json({ error: "Product not found" }, { status: 404 });
 
-  return NextResponse.json({ id: params.id, name: "Egg", price: 3 });
+  return NextResponse.json(product);
 };
 
 export const PUT = async (request: NextRequest, { params }: Props) => {
@@ -20,7 +27,7 @@ export const PUT = async (request: NextRequest, { params }: Props) => {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  if (params.id > 10)
+  if (parseInt(params.id) > 10)
     return NextResponse.json({ error: "Product not found" }, { status: 404 });
 
   return NextResponse.json({
@@ -31,7 +38,7 @@ export const PUT = async (request: NextRequest, { params }: Props) => {
 };
 
 export const DELETE = (request: NextRequest, { params }: Props) => {
-  if (params.id > 10)
+  if (parseInt(params.id) > 10)
     return NextResponse.json({ error: "Product not found" }, { status: 404 });
 
   return NextResponse.json({});
