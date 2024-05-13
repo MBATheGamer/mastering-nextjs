ce9d2223cae82ba5c242c8316fa625d5886098e5
MBATheGamer <mbathegamer@gmail.com>
2024-05-13 01:16:08 +0100

    Storing products in database (get data)

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,11 @@
+import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
-export const GET = (request: NextRequest) => {
-  return NextResponse.json([
-    { id: 1, name: "Milk", price: 2.5 },
-    { id: 2, name: "Bread", price: 3.5 },
-  ]);
+export const GET = async (request: NextRequest) => {
+  const products = await prisma.product.findMany();
+
+  return NextResponse.json(products);
 };
 
 export const POST = async (request: NextRequest) => {
