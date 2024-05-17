bc3a937a661de966892b08274976936a7506f65b
MBATheGamer <mbathegamer@gmail.com>
2024-05-17 20:46:45 +0100

    Configure Google provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
-      <Link href="/" className="mr-5">
+    <div className="flex bg-slate-200 p-4 space-x-4">
+      <Link href="/" className="mr-4">
         Next.js
       </Link>
       <Link href="/users">Users</Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
