0891680daae14623c1a65136092ff8251ef8a071
MBATheGamer <mbathegamer@gmail.com>
2024-05-21 18:24:12 +0100

    Search Engine Optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from "next";
+
+export const generateMetadata = async () => {
+  const product = await fetch("");
+
+  return {
+    title: "product.title",
+    description: "product.description",
+  } as Metadata;
+};
+
 export default async function Home() {
   return (
     <main className="relative h-screen">
