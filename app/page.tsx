40d7da4a4882b759b45e7c77c3102e0caf9affed
MBATheGamer <mbathegamer@gmail.com>
2024-05-21 06:46:12 +0100

    Optimize images

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import ProductCard from "./components/ProductCard";
+import coffee from "@/public/images/Roasted_coffee_beans.jpg";
+import Image from "next/image";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
-    <main>
-      <h1>Hello, {session && <span>{session.user?.name}</span>}!</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image src={coffee} alt="Coffee" />
+      <Image
+        className="object-cover"
+        src="https://bit.ly/react-cover"
+        alt="Cover"
+        width={300}
+        height={170}
+        fill
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
