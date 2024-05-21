6fad5dcabe19ea7b84f9bde288ae8e771fda6405
MBATheGamer <mbathegamer@gmail.com>
2024-05-21 18:44:25 +0100

    Lazy Loading (Lodash)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Metadata } from "next";
-import dynamic from "next/dynamic";
 import { useState } from "react";
 
 export const generateMetadata = async () => {
@@ -13,19 +12,25 @@ export const generateMetadata = async () => {
   } as Metadata;
 };
 
-const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-});
-
 export default function Home() {
   const [isVisible, setVisible] = useState(false);
 
   return (
     <main className="relative h-screen">
       <h1>Hello, World</h1>
-      <button onClick={() => setVisible(true)}>Show</button>
-      {isVisible && <HeavyComponent />}
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+
+          const sortedUsers = _.orderBy(users, ["name"]);
+
+          console.log(sortedUsers);
+        }}
+      >
+        Show
+      </button>
     </main>
   );
 }
