b427d73dbba0ce8e620febd5ace8a941404e8fc9
MBATheGamer <mbathegamer@gmail.com>
2024-05-21 18:39:15 +0100

    Lazy loading (Next)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
+import { useState } from "react";
 
 export const generateMetadata = async () => {
   const product = await fetch("");
@@ -9,10 +13,19 @@ export const generateMetadata = async () => {
   } as Metadata;
 };
 
-export default async function Home() {
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+
+export default function Home() {
+  const [isVisible, setVisible] = useState(false);
+
   return (
     <main className="relative h-screen">
       <h1>Hello, World</h1>
+      <button onClick={() => setVisible(true)}>Show</button>
+      {isVisible && <HeavyComponent />}
     </main>
   );
 }
